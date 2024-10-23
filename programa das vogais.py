5533e53b1c81d3796efed9d8a8eae9e2480b71a8
michaelp7a <michael_parente222@hotmail.com>
2024-10-23 14:12:54 -0300

    Create programa das vogais.py

---
+++ programa das vogais.py
@@ -0,0 +1,20 @@
+'''
+Crie um programa que leia uma palavra e mostre quantas vogais ela possui.
+'''
+
+texto = input('Digite uma palavra: ')
+vogais = 0
+VOGAIS = 'AEIOU'
+
+for letra in texto:
+    if letra in 'aeiou':
+        vogais += 1
+
+print(f'A palavra {texto} possui {vogais} vogais')
+
+print("A vogais encontradas são: ")
+for letra in texto:
+    if letra.upper() in VOGAIS:
+        print(letra, end='')
+else:
+    print('')
