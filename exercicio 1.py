69c0fc99d0d7bf1ea88b5793d2000a0bd89470d0
michaelp7a <michael_parente222@hotmail.com>
2024-10-23 15:31:06 -0300

    Create exercicio 1.py

---
+++ exercicio 1.py
@@ -0,0 +1,12 @@
+'''
+Faça um algoritmo para ler dois números e imprimir a soma dos números lidos.
+'''
+
+# Entre com o primeiro número: 10
+n1 = int(input('Entre com o primeiro número: '))
+
+# Entre com o segundo número: 20
+n2 = int(input('Entre com o segundo número: '))
+
+# Mostre a soma dos números é: 30
+print(f'A soma dos números é: {n1 + n2}')
